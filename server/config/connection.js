4b8bbcf7ef5d608f339eb89053dc9986facceb74
hmccarthy1 <huntermccarthy56@hotmail.com>
2023-06-09 18:44:08 -0400

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config({path: '../.env'});
 
 mongoose.connect(process.env.REACT_APP_MONGOOSE_STRING,
 {
+  dbName: 'FloridaSpringsDeepDiveDB',
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).catch((err) => console.log(err))
