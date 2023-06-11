7c42d9d0a5bb6dc6480f635ff7291fdbe4f1f01c
hmccarthy1 <huntermccarthy56@hotmail.com>
2023-06-11 17:03:49 -0400

    test

--- server/server.js
+++ server/server.js
@@ -29,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 
 
